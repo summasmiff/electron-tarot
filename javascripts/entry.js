2477fa17608258cf55329014e4bd77293f940da0
Summer Smith <jessicasummersmith@gmail.com>
2016-01-16 23:43:04 -0500

    use classNames to display card from props

--- javascripts/entry.js
+++ javascripts/entry.js
@@ -4,6 +4,7 @@ require('../less/main.less');
 
 var React = require('react');
 var ReactDOM = require('react-dom');
+var classNames = require('classnames');
 
 var divStyle = {
   background: 'url(../assets/background.jpg) repeat-x'
@@ -11,7 +12,10 @@ var divStyle = {
 
 var Card = React.createClass({
   render: function() {
-    return (<div className="card">{this.props.card}</div>);
+    var cardPic = classNames("card", this.props.card);
+    return (<div className={cardPic}>
+              {this.props.card}
+            </div>);
   }
 });
 
@@ -25,4 +29,4 @@ var Layout = React.createClass({
   }
 });
 
-ReactDOM.render(<Card card={'the-fool'} />, document.getElementById('content'));
+ReactDOM.render(<Card card={'the-magician'} />, document.getElementById('content'));
